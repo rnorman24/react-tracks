62ff4c66b066e66ee31017b4eeffb60c66712e43
unknown <nray24@gmail.com>
2019-05-15 10:35:38 -0600

    added routing

--- react-tracks-client/src/Root.js
+++ react-tracks-client/src/Root.js
@@ -1,20 +1,33 @@
 import React from "react";
-import withRoot from "./withRoot";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { Query } from 'react-apollo';
-import { gql } from 'apollo-boost';
+import withRoot from "./withRoot";
+import App from "./pages/App";
+import Profile from "./pages/Profile";
+import Header from "./components/Shared/Header";
 
 const Root = () => (
   <Query query={ME_QUERY}>
     {({ data, loading, error }) => {
-      if (loading) return <div>Loading</div>
-      if (error) return <div>Error</div>
+      if (loading) return <div>Loading</div>;
+      if (error) return <div>Error</div>;
 
-      return <div>{JSON.stringify(data)}</div>
+      return (
+        <Router>
+          <>
+            <Header />
+            <Switch>
+              <Route exact path="/" component={App} />
+              <Route path="/profile/:id" component={Profile} />
+            </Switch>
+          </>
+        </Router>
+      );
     }}
-
   </Query>
-)
+);
 
 const ME_QUERY = gql`
   {
@@ -24,7 +37,7 @@ const ME_QUERY = gql`
       email
     }
   }
-`
+`;
 
 // const GET_TRACKS_QUERY = gql`
 // {
