568a1ef54671244237a0d323d89fbd1e9cbcc07e
unknown <nray24@gmail.com>
2019-05-15 10:19:34 -0600

    add JWT

--- react-tracks-client/src/Root.js
+++ react-tracks-client/src/Root.js
@@ -5,7 +5,7 @@ import { Query } from 'react-apollo';
 import { gql } from 'apollo-boost';
 
 const Root = () => (
-  <Query query={GET_TRACKS_QUERY}>
+  <Query query={ME_QUERY}>
     {({ data, loading, error }) => {
       if (loading) return <div>Loading</div>
       if (error) return <div>Error</div>
@@ -16,15 +16,25 @@ const Root = () => (
   </Query>
 )
 
-const GET_TRACKS_QUERY = gql`
-{
-  tracks {
-    id
-    title
-    description
-    url
+const ME_QUERY = gql`
+  {
+    me {
+      id
+      username
+      email
+    }
   }
-}
 `
+
+// const GET_TRACKS_QUERY = gql`
+// {
+//   tracks {
+//     id
+//     title
+//     description
+//     url
+//   }
+// }
+// `
 
 export default withRoot(Root);
