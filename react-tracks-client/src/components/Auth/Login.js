ddbb7680810031a7c1b1d1aa4cbf8e75c94b0bc4
unknown <nray24@gmail.com>
2019-05-15 09:33:06 -0600

    add client state to manage auth state

--- react-tracks-client/src/components/Auth/Login.js
+++ react-tracks-client/src/components/Auth/Login.js
@@ -17,9 +17,11 @@ const Login = ({ classes, setNewUser }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (event, tokenAuth) => {
+  const handleSubmit = async (event, tokenAuth, client) => {
     event.preventDefault();
-    tokenAuth();
+    const res = await tokenAuth();
+    localStorage.setItem('authToken', res.data.tokenAuth.token);
+    client.writeData({ data: { isLoggedIn: true } });
   };
 
   return (
@@ -33,13 +35,12 @@ const Login = ({ classes, setNewUser }) => {
     <Mutation
       mutation={LOGIN_MUTATION}
       variables={{ username, password }}
-      onCompleted={data => {
-        console.log({ data })
-      }}
     >
-          {(tokenAuth, { loading, error }) => {
-            return ( 
-              <form onSubmit={event => handleSubmit(event, tokenAuth)} className={classes.form}>
+      {(tokenAuth, { loading, error, called, client }) => {
+        return ( 
+          <form
+            onSubmit={event => handleSubmit(event, tokenAuth, client)} className={classes.form}
+          >
                 <FormControl margin="normal" required fullWidth>
                   <InputLabel htmlFor="username">Username</InputLabel>
                   <Input id="username" onChange={event => setUsername(event.target.value)} />
