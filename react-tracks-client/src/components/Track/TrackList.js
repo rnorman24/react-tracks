7abd77a5700e9b3cb6bab944aa36d2feac7be884
unknown <nray24@gmail.com>
2019-05-20 11:11:57 -0600

    added track updating

--- react-tracks-client/src/components/Track/TrackList.js
+++ react-tracks-client/src/components/Track/TrackList.js
@@ -45,7 +45,7 @@ const TrackList = ({ classes, tracks }) => (
           <Typography variant="body1">{track.description}</Typography>
         </ExpansionPanelDetails>
         <ExpansionPanelActions>
-          <UpdateTrack />
+          <UpdateTrack track={track} />
           <DeleteTrack />
         </ExpansionPanelActions>
       </ExpansionPanel>
