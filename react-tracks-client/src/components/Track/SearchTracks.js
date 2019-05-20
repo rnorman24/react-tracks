726bf37077b1e8e3822d726b6e47ac36708915b6
unknown <nray24@gmail.com>
2019-05-20 09:57:18 -0600

    added search input

--- react-tracks-client/src/components/Track/SearchTracks.js
+++ react-tracks-client/src/components/Track/SearchTracks.js
@@ -1,13 +1,31 @@
 import React from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-// import TextField from "@material-ui/core/TextField";
-// import ClearIcon from "@material-ui/icons/Clear";
-// import Paper from "@material-ui/core/Paper";
-// import IconButton from "@material-ui/core/IconButton";
-// import SearchIcon from "@material-ui/icons/Search";
+import TextField from "@material-ui/core/TextField";
+import ClearIcon from "@material-ui/icons/Clear";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
 
 const SearchTracks = ({ classes }) => {
-  return <div>SearchTracks</div>;
+  return (
+    <form>
+      <Paper className={classes.root} elevation={1}>
+        <IconButton>
+          <ClearIcon />
+        </IconButton>
+        <TextField 
+          fullWidth
+          placeholder='Search All Tracks'
+          InputProps={{
+            disableUnderline: true
+          }}
+        />
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    </form>
+  )
 };
 
 const styles = theme => ({
