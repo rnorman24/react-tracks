09408f3afbbb3db6e20d34366018bd497c6a2a4d
unknown <nray24@gmail.com>
2019-05-21 10:59:55 -0600

    refactor track delete

--- react-tracks-client/src/components/Track/DeleteTrack.js
+++ react-tracks-client/src/components/Track/DeleteTrack.js
@@ -2,15 +2,15 @@ import React, { useContext } from "react";
 import IconButton from "@material-ui/core/IconButton";
 import TrashIcon from "@material-ui/icons/DeleteForeverOutlined";
 
-import { UserContext } from "../../Root"
+import { UserContext } from "../../Root";
 
 const DeleteTrack = ({ track }) => {
-  const currentUser = useContext(UserContext)
-  const isCurrentUser = currentUser.id === track.postedBy.id
-}
+  const currentUser = useContext(UserContext);
+  const isCurrentUser = currentUser.id === track.postedBy.id;
+};
 
 const DeleteTrack = () => {
-  return isCurrentUser && <div>DeleteTrack</div>
+  return isCurrentUser && <div>DeleteTrack</div>;
 };
 
 export default DeleteTrack;
