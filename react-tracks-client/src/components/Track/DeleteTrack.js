1678100f98b6e41d5a0d2cfec3d9de1e4e88896e
unknown <nray24@gmail.com>
2019-05-21 11:13:55 -0600

    added track delete mutation

--- react-tracks-client/src/components/Track/DeleteTrack.js
+++ react-tracks-client/src/components/Track/DeleteTrack.js
@@ -1,4 +1,6 @@
 import React, { useContext } from "react";
+import { Mutation } from 'react-apollo'
+import { gql } from 'apollo-boost'
 import IconButton from "@material-ui/core/IconButton";
 import TrashIcon from "@material-ui/icons/DeleteForeverOutlined";
 
@@ -8,7 +10,29 @@ const DeleteTrack = ({ track }) => {
   const currentUser = useContext(UserContext);
   const isCurrentUser = currentUser.id === track.postedBy.id;
 
-  return isCurrentUser && <div>DeleteTrack</div>;
+  return isCurrentUser && (
+    <Mutation
+    mutation={DELETE_TRACK_MUTATION}
+    variables={{ trackId: track.id }}
+    onCompleted={data => {
+      console.log({data})
+    }}
+    >
+      {deleteTrack => (
+        <IconButton onClick={deleteTrack}>
+          <TrashIcon />
+        </IconButton>
+      )}
+    </Mutation>
+  )
 };
+
+const DELETE_TRACK_MUTATION = gql`
+  mutation(trackId: Int!) {
+    deleteTrack(trackId: $trackId) {
+      trackId
+    }
+  }
+`
 
 export default DeleteTrack;
