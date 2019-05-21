843bdafd55ca8fe334f62a8e5854906ace9cec21
unknown <nray24@gmail.com>
2019-05-21 11:14:27 -0600

    refactor track delete mutation

--- react-tracks-client/src/components/Track/DeleteTrack.js
+++ react-tracks-client/src/components/Track/DeleteTrack.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
-import { Mutation } from 'react-apollo'
-import { gql } from 'apollo-boost'
+import { Mutation } from "react-apollo";
+import { gql } from "apollo-boost";
 import IconButton from "@material-ui/core/IconButton";
 import TrashIcon from "@material-ui/icons/DeleteForeverOutlined";
 
@@ -10,21 +10,23 @@ const DeleteTrack = ({ track }) => {
   const currentUser = useContext(UserContext);
   const isCurrentUser = currentUser.id === track.postedBy.id;
 
-  return isCurrentUser && (
-    <Mutation
-    mutation={DELETE_TRACK_MUTATION}
-    variables={{ trackId: track.id }}
-    onCompleted={data => {
-      console.log({data})
-    }}
-    >
-      {deleteTrack => (
-        <IconButton onClick={deleteTrack}>
-          <TrashIcon />
-        </IconButton>
-      )}
-    </Mutation>
-  )
+  return (
+    isCurrentUser && (
+      <Mutation
+        mutation={DELETE_TRACK_MUTATION}
+        variables={{ trackId: track.id }}
+        onCompleted={data => {
+          console.log({ data });
+        }}
+      >
+        {deleteTrack => (
+          <IconButton onClick={deleteTrack}>
+            <TrashIcon />
+          </IconButton>
+        )}
+      </Mutation>
+    )
+  );
 };
 
 const DELETE_TRACK_MUTATION = gql`
@@ -33,6 +35,6 @@ const DELETE_TRACK_MUTATION = gql`
       trackId
     }
   }
-`
+`;
 
 export default DeleteTrack;
