d39821edcaf0e69f8ee8e6d00355c65eb0aa9ac3
unknown <nray24@gmail.com>
2019-05-21 11:55:17 -0600

    added likes icon

--- react-tracks-client/src/components/Track/LikeTrack.js
+++ react-tracks-client/src/components/Track/LikeTrack.js
@@ -1,11 +1,38 @@
 import React from "react";
+import { Mutation } from 'react-apollo'
+import { gql } from 'apollo-boost'
 import withStyles from "@material-ui/core/styles/withStyles";
-// import IconButton from "@material-ui/core/IconButton";
-// import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import IconButton from "@material-ui/core/IconButton";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
-const LikeTrack = ({ classes }) => {
-  return <div>LikeTrack</div>;
+const LikeTrack = ({ classes, trackId, likeCount }) => {
+  return (
+    <Mutation
+    mutation={CREATE_LIKE_MUTATION}
+    variables={{ trackId }}
+    >
+      {createLike => (
+        <IconButton className={classes.iconButton}>
+          {likeCount}
+          <ThumbUpIcon className={classes.icon} />
+        </IconButton>
+      )}
+    </Mutation>
+  )
 };
+
+const CREATE_LIKE_MUTATION = gql`
+  mutation($trackId: Int!) {
+    createLike(trackId: $trackId) {
+      track {
+        id
+        likes {
+          id
+        }
+      }
+    }
+  }
+`
 
 const styles = theme => ({
   iconButton: {
